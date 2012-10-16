3dec5b1c918d45fbd189661ecbbadd8624ba8e1a
Florencia Gadea <florencia@rotterdam-cs.com>
2012-10-16 13:46:13 -0300

    initial commit

---
+++ docroot/WEB-INF/src/com/rcs/socialnetworks/portlet/SocialNetworksController.java
@@ -0,0 +1,53 @@
+package com.rcs.socialnetworks.portlet;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.portlet.PortletRequest;
+import javax.portlet.PortletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.portlet.ModelAndView;
+import org.springframework.web.portlet.bind.annotation.RenderMapping;
+
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+
+@Controller(value="SocialNetworksController") 
+@RequestMapping("VIEW")
+public class SocialNetworksController {
+	private static Log log = LogFactoryUtil.getLog(SocialNetworksController.class);
+		
+	@RenderMapping
+	public ModelAndView resolveView(PortletRequest request, PortletResponse response) throws PortalException, SystemException {
+		Map<String, Object> model = new HashMap<String, Object>();
+		String jsonContacts = " {\"person\": {" + 
+		   "\"id\": \"1\"," +
+		   "\"firstname\": \"Mariano\"," +
+		   "\"lastname\": \"Perez\"," +
+		   "\"socialnetworks\": {" +
+		     "\"socialnetwork\": [" +
+		       "{\"value\": \"linkedin\"}," +
+		       "{\"value\": \"facebook\"}," +
+		       "{\"value\": \"googleplus\"}" +
+		     "]" +
+		   "}"+
+		 "},{" +
+		   "\"id\": \"2\"," +
+		   "\"firstname\": \"Susana\"," +
+		   "\"lastname\": \"Hernandez\"," +
+		   "\"socialnetworks\": {" +
+		     "\"socialnetwork\": [" +
+		       "{\"value\": \"linkedin\"}," +
+		       "{\"value\": \"facebook\"}" +
+		     "]" +
+		   "}" +
+		 "}" +
+		"}";
+		model.put("jsonContacts", jsonContacts);
+		return new ModelAndView("view", model);
+	}
+}
