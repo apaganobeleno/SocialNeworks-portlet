54a33d2142f8943d47ee6c53c2122b990ad1b16e
Florencia Gadea <florencia@rotterdam-cs.com>
2013-04-26 13:02:12 -0300

    added twitter

--- docroot/WEB-INF/src/com/rcs/socialnetworks/contact/SocialNetworkDTO.java
+++ docroot/WEB-INF/src/com/rcs/socialnetworks/contact/SocialNetworkDTO.java
@@ -3,8 +3,12 @@ package com.rcs.socialnetworks.contact;
 import java.io.Serializable;
 
 public class SocialNetworkDTO implements Serializable {
-	
+		
 	private String socialNetworkName;
+	
+	public SocialNetworkDTO(String socialNetworkName) {
+		this.socialNetworkName = socialNetworkName;
+	}
 
 	public String getSocialNetworkName() {
 		return socialNetworkName;
